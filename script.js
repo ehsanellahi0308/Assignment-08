4c03f2ec5ed6771266d57104ecc5faca1a4d28af
ehsanellahi0308 <ehsanvicky0@gmail.com>
2025-01-04 17:26:28 +0500

    Assignment 08

---
+++ script.js
@@ -0,0 +1,16 @@
+var userinput = prompt ("user input");
+if(userinput >= 70){
+    console.log("grad A")
+}
+else if(userinput >= 60) {
+    console.log("grad B");
+}
+else if(userinput >= 50) {
+    console.log("grad C");
+}
+else if(userinput >= 40) {
+    console.log("grad D");
+}
+else{
+    console.log("grad f");
+}
