db15aca83fb068961c51ffc94f914a483e9b0d50
ehsanellahi0308 <ehsanvicky0@gmail.com>
2025-01-05 14:27:31 +0500

    print number of days in that month

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var userinput = prompt ("user input");
+
+/*var userinput = prompt ("user input");
 if(userinput >= 70){
     console.log("grad A")
 }
@@ -13,4 +14,94 @@ else if(userinput >= 40) {
 }
 else{
     console.log("grad f");
-}
+}*/
+
+
+/*Write a js program to input week number and print week day*/
+
+/*var inputuser = prompt("enter a number between 1 to 7")
+switch(inputuser){
+    case "1":{
+        console.log("monday");
+        break;
+    }
+    case "2":{
+        console.log("Tuesday");
+        break;
+    }
+    case "3":{
+        console.log("Wednesday");
+        break;
+    }
+    case "4":{
+        console.log("Thursday");
+        break;
+    }
+    case "5":{
+        console.log("Friday");
+        break;
+    }
+    case "6":{
+        console.log("Saturday");
+        break;
+    }
+    case "7":{
+        console.log("sunday");
+        break;
+    }
+}*/
+
+/*Write a js program to input month number and print number of days in that month.*/
+var inputuser =("enter a number between 1 to 12");
+switch(inputuser){
+    case "1":{
+        console.log("january");
+        break;
+    }
+    case "2":{
+        console.log("february");
+        break;
+    }
+    case "3":{
+        console.log("march");
+        break;
+    }
+    case "4":{
+        console.log("april");
+        break;
+    }
+    case "5":{
+        console.log("may");
+        break;
+    }
+    case "6":{
+        console.log("june");
+        break;
+    }
+    case "7":{
+        console.log("july");
+        break;
+    }    
+    case "8":{
+        console.log("august");
+        break;
+    }    
+    case "9":{
+        console.log("September");
+        break;
+    }    
+    case "10":{
+        console.log("october");
+        break;
+    }    
+    case "11":{
+        console.log("november");
+        break;
+    }
+    case "12":{
+        console.log("December");
+        break;
+    }
+        
+        
+    }
